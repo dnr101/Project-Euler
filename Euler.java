3aec387ba0a7976056ca7b85b9d0f9903efb4fdd
David Raizen <dnr101@gmail.com>
2011-12-01 10:13:26 -0500

    12-01-2011 1

--- Euler.java
+++ Euler.java
@@ -347,6 +347,14 @@ public class Euler {
 		
 	}
 	
+	public static int[] pythagTrip(int m, int n){
+		int[] trip = new int[3];
+		trip[0] = Math.abs((int)Math.pow(m, 2) - (int)Math.pow(n, 2));
+		trip[1] = Math.abs(2 * m * n);
+		trip[2] = Math.abs((int)Math.pow(m, 2) + (int)Math.pow(n, 2));
+		return trip;
+	}
+	
 	public static boolean isPrime(long x){
 		if (x < 2){
 			return false;
@@ -635,17 +643,49 @@ public class Euler {
 	}
 
 	private void num9() {
-		// TODO Auto-generated method stub
+		int[] searchTrip = new int[3];
+		for (int i = 0; i < 50; i++){
+			for (int j = 0; j < 50; j++){
+				if (i == j){
+					continue;
+				}
+				searchTrip = pythagTrip(i, j);
+				if (searchTrip[0]+searchTrip[1]+searchTrip[2] == 1000){
+					solution = (searchTrip[0]*searchTrip[1]*searchTrip[2]);
+				}
+			}
+		}
 	}
 
 	private void num8() {
-		// TODO Auto-generated method stub
+		String number = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+		ArrayList<Integer> prods = new ArrayList<Integer>();
+		char[] slice;
+		int prod;
+		for (int i = 0; i < number.length()-5; i++){
+			prod = 1;
+			slice = (number.substring(i, i+5).toCharArray());
+			for (char p : slice){
+				prod *= Integer.parseInt("" + p);
+			}
+			prods.add(prod);
+		}
+		solution = Collections.max(prods);
 		
 	}
 
 	private void num7() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("working...");
+		int pcount = 1;
+		solution = 3;
+		while (pcount < 10001){
+			if (isPrime(solution)){
+				pcount++;	
+			}
+			solution += 2;
+		}
+		solution -= 2;
+		System.out.println(solution);
 	}
 
 	private void num6() {
